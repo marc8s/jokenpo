ce9cdf9cf5bbdfe740f1c16b77e03eb2bd0d3d2e
Marc8s <marcus.ecomp@gmail.com>
2021-03-16 16:18:41 +0000

    Lógica de vitoria, derrota, empate

--- app/src/main/java/com/example/jokenpo/MainActivity.java
+++ app/src/main/java/com/example/jokenpo/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -25,9 +26,10 @@ public class MainActivity extends AppCompatActivity {
     public void scissorSelected(View view){
         this.optionSelected("scissor");
     }
-    public void optionSelected(String optionSelected){
+    public void optionSelected(String optionUser){
         ImageView imageResult = findViewById(R.id.imageview_escolha_do_app);
-        int randomAppPlayer = new Random().nextInt(3);//0, 1, 2
+        TextView textResult = (TextView) findViewById(R.id.texview_resultado);
+         int randomAppPlayer = new Random().nextInt(3);//0, 1, 2
         String[] options = {"rock", "paper", "scissor"};
         String optionApp =  options[randomAppPlayer];
 
@@ -41,6 +43,22 @@ public class MainActivity extends AppCompatActivity {
             case "scissor":
                 imageResult.setImageResource(R.drawable.tesoura);
                 break;
+        }
+
+        //app wins
+        if(optionApp == "paper" && optionUser == "rock" ||
+                optionApp == "rock" && optionUser == "scissor" ||
+                optionApp == "scissor" && optionUser == "paper"){
+            textResult.setText(R.string.voce_perdeu);
+        //user wins
+        }else if(optionUser == "paper" && optionApp == "rock" ||
+                optionUser == "rock" && optionApp == "scissor" ||
+                optionUser == "scissor" && optionApp == "paper"){
+            textResult.setText(R.string.voce_venceu);
+
+        //tie
+        }else{
+            textResult.setText(R.string.empate);
         }
     }
 }
