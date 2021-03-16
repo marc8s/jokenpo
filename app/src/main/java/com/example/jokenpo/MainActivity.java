90023ceae8b90f2644d93eae3fe6c30e42454822
Marc8s <marcus.ecomp@gmail.com>
2021-03-16 15:50:38 +0000

    Detecção dos eventos de click

--- app/src/main/java/com/example/jokenpo/MainActivity.java
+++ app/src/main/java/com/example/jokenpo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jokenpo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void rockSelected(View view){
+        this.optionSelected("rock");
+    }
+    public void paperSelected(View view){
+        this.optionSelected("paper");
+    }
+    public void scissorSelected(View view){
+        this.optionSelected("scissor");
+    }
+    public void optionSelected(String optionSelected){
+
     }
 }
